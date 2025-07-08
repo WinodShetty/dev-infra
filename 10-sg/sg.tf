be5acfd4a3ff48685db1527cf31a9888ed8e5add
vinod <mechvnod@gmail.com>
2025-07-08 21:57:10 +0530

    sg

---
+++ 10-sg/sg.tf
@@ -0,0 +1,11 @@
+module "mysql_sg" {
+    source = "git::https://github.com/DAWS-82S/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+
