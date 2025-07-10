46fac1433f8370f9b9a5c83078a491091e02d1a0
vinod <mechvnod@gmail.com>
2025-07-10 16:00:06 +0530

    sg

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -18,7 +18,8 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
 
 resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
-  
+  type  = "String"
+
 }
 
 
