d5f05cc350c2d51450a7936f41d532273a5a90fb
vinod <mechvnod@gmail.com>
2025-07-10 15:58:33 +0530

    sg

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -10,4 +10,9 @@ resource "aws_ssm_parameter" "backend_sg_id" {
   value = module.backend_sg.sg_id
 }
 
+resource "aws_ssm_parameter" "frontend_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/frontend_sg_id"
+  type  = "String"
+  value = module.frontend_sg.sg_id
+}
 
