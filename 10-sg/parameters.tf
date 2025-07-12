10755cc2d73d0b1e7e8a917fb76dff7a8dc050f6
vinod <mechvnod@gmail.com>
2025-07-12 16:15:46 +0530

    sg

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -20,7 +20,7 @@ resource "aws_ssm_parameter" "alb_ingress_sg_id" {
 resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
   name  = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
   type  = "String"
-  
+  value = module.eks_control_plane_sg.sg_id
 }
 
 resource "aws_ssm_parameter" "eks_node_sg_id" {
