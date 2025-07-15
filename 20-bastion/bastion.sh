870d17ab0ad573ed6b5ae6bcd80a805cb20cc5f5
vinod <mechvnod@gmail.com>
2025-07-15 21:51:38 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -17,3 +17,4 @@ systemctl enable docker
 usermod -aG docker ec2-user
 #kubectl installation
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+chmod +x ./kubectl
