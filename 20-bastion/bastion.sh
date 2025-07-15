77e39eaee10badfb60345e51f7eb922e8ca21d06
vinod <mechvnod@gmail.com>
2025-07-15 21:49:55 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -10,3 +10,5 @@ xfs_growfs /
 xfs_growfs /var
 
 dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
