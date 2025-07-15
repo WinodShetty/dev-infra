02d9e0507482ca358ee0081319cf2c8ec03278c3
vinod <mechvnod@gmail.com>
2025-07-15 21:50:36 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -14,3 +14,4 @@ dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
+usermod -aG docker ec2-user
