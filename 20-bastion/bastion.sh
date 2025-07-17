bf8ac8416da18234894b741eb927af92656ae2aa
vinod <mechvnod@gmail.com>
2025-07-17 19:11:45 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -32,3 +32,4 @@ chmod 700 get_helm.sh
 
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
