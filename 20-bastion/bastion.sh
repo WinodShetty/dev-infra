31d4c76bff68c6b911a55b446462ad11a0ece420
vinod <mechvnod@gmail.com>
2025-07-17 19:11:18 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -29,3 +29,5 @@ mv /tmp/eksctl /usr/local/bin
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
