8f34fd41310dbd219abf2a42ee97f63d55cd5cd9
vinod <mechvnod@gmail.com>
2025-07-17 19:12:02 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -33,3 +33,5 @@ chmod 700 get_helm.sh
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+dnf install mysql -y
