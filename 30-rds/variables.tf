1496e79d1a26a9f1ee97e211443d0c825470b2d9
vinod <mechvnod@gmail.com>
2025-07-20 17:52:08 +0530

    rds

--- 30-rds/variables.tf
+++ 30-rds/variables.tf
@@ -18,3 +18,23 @@ variable "common_tags" {
     }
 }
 
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z021448929NMXW4P65UQE"
+}
+
