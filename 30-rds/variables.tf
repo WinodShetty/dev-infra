e9ee9e9ae1071768b66b0fa9c40731b0bb6d3954
vinod <mechvnod@gmail.com>
2025-07-20 17:51:58 +0530

    rds

--- 30-rds/variables.tf
+++ 30-rds/variables.tf
@@ -2,3 +2,19 @@ variable "project_name" {
     default = "roboshop"
 }
 
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
