87a700813d712c33898c1f8309e32b135143f278
vinod <mechvnod@gmail.com>
2025-08-03 14:12:33 +0530

    dev-infra

--- 60-backend/locals.tf
+++ 60-backend/locals.tf
@@ -2,5 +2,6 @@ locals {
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   resource_name = "${var.project_name}-${var.environment}-backend"
-  
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+ 
 }
