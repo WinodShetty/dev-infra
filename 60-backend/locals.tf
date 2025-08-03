eedada6c3eeddbf249a5e448005831f1c1727338
vinod <mechvnod@gmail.com>
2025-08-03 14:12:24 +0530

    dev-infra

--- 60-backend/locals.tf
+++ 60-backend/locals.tf
@@ -1,5 +1,6 @@
 locals {
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  resource_name = "${var.project_name}-${var.environment}-backend"
   
 }
