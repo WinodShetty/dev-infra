b10a81a0c466d74d268e41706cd3804ea1d6aa05
vinod <mechvnod@gmail.com>
2025-08-13 20:55:55 +0530

    dev-infra

--- 90-frontend/main.tf
+++ 90-frontend/main.tf
@@ -216,3 +216,16 @@ resource "aws_launch_template" "frontend" {
     }
   }
 }
+
+resource "aws_autoscaling_group" "frontend" {
+  name                      = local.resource_name
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 180 # 3 minutes for instance to intialise
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  target_group_arns = [aws_lb_target_group.frontend.arn]
+  launch_template {
+    id      = aws_launch_template.frontend.id
+    version = "$Latest"
+  }
