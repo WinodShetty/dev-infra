43967bf54fcd4f57985a8378184185a3c2676391
vinod <mechvnod@gmail.com>
2025-08-15 10:01:42 +0530

    dev-infra

--- 90-frontend/locals.tf
+++ 90-frontend/locals.tf
@@ -2,5 +2,6 @@ locals {
   public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
   public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
   resource_name = "${var.project_name}-${var.environment}-frontend"
- 
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  
 }
