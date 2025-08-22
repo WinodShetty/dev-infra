58f911ada993e44ffdeeeb25c9331c2b83843b12
vinod <mechvnod@gmail.com>
2025-08-22 15:47:28 +0530

    web-alb

--- 50-webalb/main.tf
+++ 50-webalb/main.tf
@@ -5,5 +5,6 @@ module "alb" {
   name    = "${var.project_name}-${var.environment}-app-alb"
   vpc_id  = data.aws_ssm_parameter.vpc_id.value
   subnets = local.private_subnet_ids
- 
+  create_security_group = false
+
 }
