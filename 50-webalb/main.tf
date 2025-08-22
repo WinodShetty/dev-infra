dc70cc630dfa5af6e198e54f0fc43aac3f2f8bcf
vinod <mechvnod@gmail.com>
2025-08-22 15:46:07 +0530

    web-alb

--- 50-webalb/main.tf
+++ 50-webalb/main.tf
@@ -1,5 +1,8 @@
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
   internal = true
-
+  # expense-dev-app-alb
+  name    = "${var.project_name}-${var.environment}-app-alb"
+  vpc_id  = data.aws_ssm_parameter.vpc_id.value
+  
 }
