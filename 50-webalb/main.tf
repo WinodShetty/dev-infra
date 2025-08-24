71f2a6c8bb57f87df4319c1713b426f90eb0c6d5
vinod <mechvnod@gmail.com>
2025-08-24 18:10:17 +0530

    web-alb

--- 50-webalb/main.tf
+++ 50-webalb/main.tf
@@ -37,4 +37,9 @@ resource "aws_route53_record" "app_alb" {
   zone_id = var.zone_id
   name    = "*.app-dev.${var.domain_name}"
   type    = "A"
+    alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
 }
