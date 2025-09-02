4d6320bd7cf0766f8ce08597f27a9ca51d76506c
vinod <mechvnod@gmail.com>
2025-09-02 11:25:38 +0530

    dev-infra

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -17,6 +17,6 @@ resource "aws_route53_record" "expense" {
     for dvo in aws_acm_certificate.expense.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
-      
+      type   = dvo.resource_record_type
     }
   }
