4849582155fb9a093e845d6090984c0984c91e87
vinod <mechvnod@gmail.com>
2025-09-02 11:24:33 +0530

    dev-infra

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -1,6 +1,13 @@
 resource "aws_acm_certificate" "expense" {
   domain_name       = "*.${var.domain_name}"
   validation_method = "DNS"
+  
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}"
+    }
+  )
 
   
 }
