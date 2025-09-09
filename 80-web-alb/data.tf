1c470460e457a2238ab49c7c724be79cc2ce24c6
vinod <mechvnod@gmail.com>
2025-09-09 13:26:08 +0530

    dev-infra

--- 80-web-alb/data.tf
+++ 80-web-alb/data.tf
@@ -1,3 +1,7 @@
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+}
